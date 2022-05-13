13525f8b5ab71ba7c0bc4cafe83d67cbd6815450
Nikita Zanella <n.zanella@evolve-design.com>
2022-05-13 18:19:57 +0200

    Fixed context

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { ContextProvider } from "./utils/ContextProvider";
 
 export default function App() {
-  console.log('pippooooooo')
   return (
-      <p>pippo</p>
+    <ContextProvider>
+      <p>Pippoo</p>
+    </ContextProvider>
   );
 }
--- src/utils/ContextProvider.jsx
+++ src/utils/ContextProvider.jsx
@@ -4,18 +4,7 @@ export const Context = createContext();
 export const ContextProvider = ({ children }) => {
   const [filters, setFilters] = useState();
 
-  //let [searchParams, setSearchParams] = useSearchParams();
-
-  function checkParams() {
-    //let user = searchParams.get("user");
-    return setFilters('Paperino');
-  }
-
-  useEffect(() => {
-    checkParams();
-    console.log(filters)
-  }, []);
-
+console.log('paperino')
   return (
     <Context.Provider value={[filters, setFilters]}>
       {children}
